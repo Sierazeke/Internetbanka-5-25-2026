b50112d49751339ccb873d7e69e4e7887f639084
user <lastovskisaleksis@gmail.com>
2026-05-25 10:56:40 +0300

    Made simple program to add/withdraw money, check balance and exit

--- Main.java
+++ Main.java
@@ -11,5 +11,47 @@ class Main {
         System.out.println("2 - Withdraw Money");
         System.out.println("3 - Check Balance");
         System.out.println("4 - Exit");
+
+        while (true) {
+            System.out.print("Input the action: ");
+            int userAction = scanner.nextInt();
+
+            // Add money to the balance
+            if (userAction == 1) {
+                System.out.print("How much would you like to add: ");
+                float userAdd = scanner.nextFloat(); // Reads user input
+                if (userAdd > 0) { // Check if user writes a positive amount
+                    balance = balance + userAdd;
+                }
+                else {
+                    System.out.println("Please use a valid amount!");
+                    continue; 
+                }
+            }
+
+            // Withdraw money from the balance
+            if (userAction == 2) {
+                System.out.print("How much would you like to withdraw: ");
+                float userWithdraw = scanner.nextFloat();
+                if (userWithdraw > 0) {
+                    balance = balance - userWithdraw;                
+                }
+                else {
+                    System.out.println("Please use a valid amount!");
+                    continue;
+                }
+            }
+
+            // Check the balance
+            if (userAction == 3) {
+                System.out.println("Your balance is: " + balance);
+            }
+
+            // Exits the program
+            if (userAction == 4) {
+                System.out.println("-- Goodbye! --");
+                break; // Stops the program
+            }
+        }
     }  
 }
