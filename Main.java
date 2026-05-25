fadca2902a0362a8a4807b10a53556268f71aa3a
user <lastovskisaleksis@gmail.com>
2026-05-25 10:40:26 +0300

    Main commit with simple text to guide the user

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Creates a new Scanner object
+        float balance = 100; // User balance
+
+        System.out.println("----- Greetings! -----");
+        System.out.println("- Available Actions -");
+        System.out.println("1 - Add Money");
+        System.out.println("2 - Withdraw Money");
+        System.out.println("3 - Check Balance");
+        System.out.println("4 - Exit");
+    }  
+}
